7b5b0c7d5f16fd84af8c9ddeecd2a607b4a98e9b
Ben Santoso <bbensantoso21@gmail.com>
2026-02-07 11:40:49 +0700

    Update

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -22,7 +22,7 @@
 
   <meta name="google-site-verification" content="fWnter7hioZcQFqIw4Z3aCd2fCcUHmAYLNnU3uEQMm4" />
 
-  <link rel="canonical" href="{{ 'https://lovilink.com/' . url()->full()) }}">
+  <link rel="canonical" href="{{ 'https://lovilink.com/' . url()->full() }}">
   
   @vite(['resources/css/app.css', 'resources/js/app.js'])
   @livewireStyles
