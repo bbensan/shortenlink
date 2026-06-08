e609052334cac0415a8bb77183cf04ec32f03ebf
Ben Santoso <bbensantoso21@gmail.com>
2026-06-08 21:39:55 +0700

    Added health route

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { ShortenLinksModule } from './shorten-links/shorten-links.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, ShortenLinksModule],
+  imports: [
+    DatabaseModule, 
+    AuthModule, 
+    ShortenLinksModule,
+    HealthModule
+  ],
 })
 export class AppModule {}
